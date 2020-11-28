a1c1dd351a203fd7a95b945aa4bc995089c24c65
ipdmartins <ipdmartins@gmail.com>
2020-11-28 13:18:55 -0200

    new updates to code

--- myaccount.js
+++ myaccount.js
@@ -1,11 +1,12 @@
 
 // Example uses Hedera JavaScript SDK v1.1.8
-const { Client } = require("@hashgraph/sdk");
+const { Client, AccountBalanceQuery } = require("@hashgraph/sdk");
 require("dotenv").config();
 
 // Grab your account ID and private key from the .env file
 const operatorAccountId = process.env.OPERATOR_ID;
-const operatorPrivateKey = process.env.OPERATOR_KEY;
+const operatorPrivateKey = process.env.OPERATOR_PRIVATE_KEY;
+const operatorPublicKey = process.env.OPERATOR_PUBLIC_KEY;
 
 // If we weren't able to grab it, we should throw a new error
 if (operatorPrivateKey == null || operatorAccountId == null) {
@@ -21,6 +22,7 @@ client.setOperator(operatorAccountId, operatorPrivateKey);
 const myaccount = {
     operatorAccountId,
     operatorPrivateKey,
+    operatorPublicKey,
     client
 }
 
--- wallet.js
+++ wallet.js
@@ -1,50 +1,67 @@
 const { Ed25519PrivateKey, AccountCreateTransaction, AccountInfoQuery,
-    AccountUpdateTransaction, CryptoTransferTransaction, AccountRecordsQuery } = require("@hashgraph/sdk");
-const { client, operatorAccountId } = require('./myaccount');
+    AccountUpdateTransaction, CryptoTransferTransaction, AccountRecordsQuery,
+    AccountBalanceQuery } = require("@hashgraph/sdk");
+const { client, operatorAccountId, operatorPrivateKey, operatorPublicKey } = require('./myaccount');
 const framework = require("./framework");
 
 var txconfirmedcount = 0;
 
 async function newAccount() {
+    const myAccountBalance = await new AccountBalanceQuery()
+        .setAccountId(operatorAccountId)
+        .execute(client);
+
+    console.log(`MY ACCOUNT ID: ${operatorAccountId}`);
+    console.log(`MY ACCOUNT BALANCE: ${myAccountBalance}`);
+    console.log(`MY PRIVATE KEY: ${operatorPrivateKey}`);
+    console.log(`MY PUBLIC KEY: ${operatorPublicKey}`);
 
     const privateKey = await Ed25519PrivateKey.generate();
 
-    console.log("Automatic signing example");
-    console.log(`Private key = ${privateKey.toString()}`);
-    console.log(`Public key = ${privateKey.publicKey.toString()}`);
+    console.log(`NEW PRIVATE KEY: ${privateKey.toString()}`);
+    console.log(`NEW PUBLIC KEY: ${privateKey.publicKey.toString()}`);
 
     //Creating an account
     const transactionId = await new AccountCreateTransaction()
         .setKey(privateKey.publicKey)
-        .setInitialBalance(0)
-        //.setGenerateRecord(true)
+        .setInitialBalance(1000)
         .execute(client);
 
-    var antes = Date.now();//registra o início do processo de transação
-    console.log(antes);
+    // var antes = Date.now();//registra o início do processo de transação
+    // console.log(antes);
     const transactionRecord = await transactionId.getRecord(client);
 
     const transactionReceipt = await transactionId.getReceipt(client);
 
-    //se a transação foi efetivada, tx confirmadas adiciona 1
-    if (transactionRecord.receipt.status == "SUCCESS") {
-        txconfirmedcount++;
-        console.log(txconfirmedcount);
-        //registra o momento fim da transação
-        console.log(transactionRecord.consensusTimestamp.seconds);
-    } else {
-        console.log("fail")
-    }
-    var depois = Date.now();//registra o fim do processo de transação
-    console.log(depois);
-    //fórmula do framework implementada "transacoes por segundo"
-    const TPS = framework.analyzeTPS(txconfirmedcount, antes, depois);
-    console.log("Transações por segundo: ", TPS);
-
-
     const newAccountId = transactionReceipt.getAccountId();
-    console.log(`New accountId = ${newAccountId}`);
 
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    console.log("NEW ACCOUNT ID: " + newAccountId);
+    console.log("NEW ACCOUNT BALANCE: " + accountBalance);
+
+    ///////// FRAMEWORK ////////////////////
+
+    //se a transação foi efetivada, tx confirmadas adiciona 1
+    // if (transactionRecord.receipt.status == "SUCCESS") {
+    //     txconfirmedcount++;
+    //     console.log(txconfirmedcount);
+        //registra o momento fim da transação
+    //     console.log(transactionRecord.consensusTimestamp.seconds);
+    // } else {
+    //     console.log("fail")
+    // }
+    // var depois = Date.now();//registra o fim do processo de transação
+    // console.log(depois);
+    //fórmula do framework implementada "transacoes por segundo"
+    // const TPS = framework.analyzeTPS(txconfirmedcount, antes, depois);
+    // console.log("Transações por segundo: ", TPS);
+
+    ///////// FRAMEWORK ////////////////////
+
+    transfer(newAccountId);
     //updateAccount(newAccountId, privateKey);
 }
 
@@ -84,39 +101,47 @@ async function updateAccount(newAccountId, privateKey) {
 
 async function transfer(newAccountId) {
     const receipt = await (await new CryptoTransferTransaction()
-        .addSender(operatorAccountId, 1)
-        .addRecipient(newAccountId, 1)
+        .addSender(operatorAccountId, 100)
+        .addRecipient(newAccountId, 100)
         .setTransactionMemo("sdk example")
         .execute(client))
         .getReceipt(client);
 
-    console.log("Transaction's Receipt: ", receipt);
-    console.log("Sender's balance after transfer:", (await client.getAccountBalance(operatorAccountId)));
-    console.log("Receiver's balance after transfer:", (await client.getAccountBalance(newAccountId)));
+    const myNewAccountBalance = await new AccountBalanceQuery()
+        .setAccountId(operatorAccountId)
+        .execute(client);
+
+    const receiverNewAccountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+    
+    console.log("SENDER'S BALANCE AFTER TRANSFER: ", myNewAccountBalance);
+    console.log("RECEIVER'S BALANCE AFTER TRANSFER: ", receiverNewAccountBalance);
+    console.log("TRANSACTION RECEIPT: ", receipt);
 
     const info = await new AccountInfoQuery()
         .setAccountId(operatorAccountId)
         .execute(client);
 
-    console.log(`Operator account (${operatorAccountId}) info = ${JSON.stringify(info, null, 4)}`);
+    console.log(`OPERATOR ACCOUNT (${operatorAccountId}) INFO QUERY = ${JSON.stringify(info, null, 4)}`);
 
     const infoII = await new AccountInfoQuery()
         .setAccountId(newAccountId)
         .execute(client);
 
-    console.log(`New operator account (${newAccountId}) info = ${JSON.stringify(infoII, null, 4)}`);
+    console.log(`NEW OPERATOR ACCOUNT (${newAccountId}) INFO QUERY = ${JSON.stringify(infoII, null, 4)}`);
 
-    const accountRecord = new AccountRecordsQuery()
-        .setAccountId(operatorAccountId)
-        .execute(client);
-    console.log(`Operator account (${operatorAccountId}) info = ${JSON.stringify(accountRecord
+    // const accountRecord = await new AccountRecordsQuery()
+    //     .setAccountId(operatorAccountId)
+    //     .execute(client);
+    // console.log(`OPERATOR ACCOUNT (${operatorAccountId}) INFO RECORD QUERY = ${JSON.stringify(accountRecord
 
-    )}`);
+    // )}`);
 
-    const accountRecordII = new AccountRecordsQuery()
-        .setAccountId(newAccountId)
-        .execute(client);
-    console.log(`Operator account (${newAccountId}) info = ${JSON.stringify(accountRecordII, null, 4)}`);
+    // const accountRecordII = await new AccountRecordsQuery()
+    //     .setAccountId(newAccountId)
+    //     .execute(client);
+    // console.log(`NEW OPERATOR ACCOUNT (${newAccountId}) INFO RECORD QUERY = ${JSON.stringify(accountRecordII, null, 4)}`);
 
 }
 
