febd740274d14c43634add0b26d60e8355cdb1c4
ipdmartins <ipdmartins@gmail.com>
2020-12-10 00:15:56 -0200

    some tests

--- wallet.js
+++ wallet.js
@@ -1,14 +1,15 @@
 const { Ed25519PrivateKey, AccountCreateTransaction, AccountInfoQuery,
     AccountUpdateTransaction, CryptoTransferTransaction, AccountRecordsQuery,
     AccountBalanceQuery } = require("@hashgraph/sdk");
+const si = require('systeminformation');
+const process = require('process');
+
 const { client, operatorAccountId, operatorPrivateKey, operatorPublicKey } = require('./myaccount');
 const frameworkAnalyzer = require("./frameworkAnalyzer");
-const si = require('systeminformation');
-const process = require('process'); 
-
 
 var txconfirmedcount = 0;
 var sumTxInputTxComfirmed = 0;
+var receipt = null;
 
 async function newAccount() {
     const myAccountBalance = await new AccountBalanceQuery()
@@ -50,30 +51,30 @@ async function newAccount() {
     console.log("NEW ACCOUNT BALANCE: " + newAccountBalance);
     //DELETE IN REAL TEST
 
-    //transfer(newAccountId);
+    transfer(newAccountId, 5);
     //updateAccount(newAccountId, newPrivateKey);
 }
 
 async function transfer(receiverAccountId, numberOfTransactions) {
     ///////// referent to analyzeTPC  /////////
-    var starCpuUsage = process.cpuUsage()
+    var starCpuUsage = await process.cpuUsage()
+    console.log('starCpuUsage: '+ starCpuUsage.user);
     ///////// referent to analyzeTPC  /////////
 
     ///////// referent to analyzeTPND  /////////
-
-    const rawDISKR = raw
-    ///////// referent to analyzeTPND  /////////
-
-    ///////// referent to analyzeTPND  /////////
-    const previousUPLOAD = si.networkStats().then(data => data[0].tx_sec)
-    const previousDOWNLOAD = si.networkStats().then(data => data[0].rx_sec)
+    const previousUPLOAD = await si.networkStats().then(data => data[0].tx_sec)
+    const previousDOWNLOAD = await si.networkStats().then(data => data[0].rx_sec)
+    console.log('previousUPLOAD: '+ previousUPLOAD);
+    console.log('previousDOWNLOAD: '+ previousDOWNLOAD);
     ///////// referent to analyzeTPND  /////////
 
     const before = Date.now();//get the transaction beginning for analyzeTPS
+    console.log('before: '+ before);
 
+    var receipt = null;
     for (let index = 0; index < numberOfTransactions; index++) {
         var txInput = Date.now();//it's for analyzeARD
-        const receipt = await (await new CryptoTransferTransaction()
+        const transation = await (await new CryptoTransferTransaction()
             .addSender(operatorAccountId, 100)
             .addRecipient(receiverAccountId, 100)
             .setTransactionMemo("sdk example")
@@ -82,35 +83,45 @@ async function transfer(receiverAccountId, numberOfTransactions) {
         // .getRecord(client)
 
         //se a transação foi efetivada, tx confirmadas adiciona 1
-        if (receipt.receipt.status == "SUCCESS") {
+        if (transation.status == "SUCCESS") {
             var txConfirmed = Date.now();//it's for analyzeARD
             sumTxInputTxComfirmed += (txConfirmed - txInput)//it's for analyzeARD
             txconfirmedcount++;
             // console.log(txconfirmedcount);
         } else {
-            console.log(`transaction ${index+1} failed.`)
+            console.log(`transaction ${index + 1} failed.`)
+        }
+        if(index == numberOfTransactions){
+            receipt = await transation.getReceipt(client);
         }
     }
     const after = Date.now();//get the transaction's end analyzeTPS
+    console.log('after: '+ after);
 
     ///////// referent to analyzeTPC  /////////
-    const cpuUsageByTheProcess = process.cpuUsage(starCpuUsage)
+    const cpuUsageByTheProcess = await process.cpuUsage(starCpuUsage)
     const coreFrequency = await si.cpuCurrentspeed().then(data => data.cores[0]);
+    console.log('cpuUsageByTheProcess: '+ cpuUsageByTheProcess.user);
+    console.log('coreFrequency: '+ coreFrequency);
     ///////// referent to analyzeTPC  /////////
-    
+
     ///////// referent to analyzeTPND  /////////
-    const postUPLOAD = si.networkStats().then(data => data[0].tx_sec)
-    const postDOWNLOAD = si.networkStats().then(data => data[0].rx_sec)
+    const postUPLOAD = await si.networkStats().then(data => data[0].tx_sec)
+    const postDOWNLOAD = await si.networkStats().then(data => data[0].rx_sec)
+    console.log('postUPLOAD: '+ postUPLOAD);
+    console.log('postDOWNLOAD: '+ postDOWNLOAD);
 
     const UPLOAD = postUPLOAD - previousUPLOAD;
     const DOWNLOAD = postDOWNLOAD - previousDOWNLOAD;
+    console.log('UPLOAD: '+ UPLOAD);
+    console.log('DOWNLOAD: '+ DOWNLOAD);
     ///////// referent to analyzeTPND  /////////
-    
-    console.log(receipt.conconsensusTimestamp.seconds);
+
+    // console.log(receipt.conconsensusTimestamp.seconds);
 
     const TPS = frameworkAnalyzer.analyzeTPS(txconfirmedcount, before, after);
     console.log("Transacoes por segundo: ", TPS);
-    
+
     const ARD = frameworkAnalyzer.analyzeARD(sumTxInputTxComfirmed, txconfirmedcount)
     console.log("Media do atraso de resposta: ", ARD);
 
@@ -120,17 +131,18 @@ async function transfer(receiverAccountId, numberOfTransactions) {
 
     //Não sei se é possível achar a memoria usada em blockchain e a memoria virtual real???
     //investigar o Kabuto
-    const TPMS = frameworkAnalyzer.analyzeTPMS(txconfirmedcount, RMEM, VMEM)
-    console.log("Transacoes de memoria por segundo: ", TPMS);
+    // const TPMS = frameworkAnalyzer.analyzeTPMS(txconfirmedcount, RMEM, VMEM)
+    // console.log("Transacoes de memoria por segundo: ", TPMS);
 
-    //buscar DISKR e o DISKW com o netdata
-    const TPDIO = frameworkAnalyzer.analyzeTPDIO(txconfirmedcount, DISKR, DISKW)
-    console.log("Transacoes por disco: ", TPDIO);
+    //buscar DISKR e o DISKW com o netdata????????
+    // const TPDIO = frameworkAnalyzer.analyzeTPDIO(txconfirmedcount, DISKR, DISKW)
+    // console.log("Transacoes por disco: ", TPDIO);
 
     //buscar UPLOAD e o DOWNLOAD com o netdata
     const TPND = frameworkAnalyzer.analyzeTPND(txconfirmedcount, UPLOAD, DOWNLOAD)
     console.log("Transacoes de dados na rede: ", TPND);
 
+    accountRecords(receiverAccountId, receipt)
 }
 
 async function accountRecords(receiverAccountId, receipt) {
